9c750b0de1620d811260535d5442bd9f1c94bd1f
md5502 <m.baniasadi.d@gmail.com>
2024-09-17 22:40:09 +0330

    add the search api url to the main urlpatterns

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,9 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/", include("api.urls", namespace="api")),
+
 ]
